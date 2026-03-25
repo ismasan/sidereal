b181ec26adc5ed8b2e9d63e1e27a46e574c4216c
Ismael Celis <ismaelct@gmail.com>
2026-03-25 14:52:11 +0000

    LLM chat demo

--- examples/chat/app.rb
+++ examples/chat/app.rb
@@ -1,10 +1,29 @@
 # frozen_string_literal: true
 
-require 'sidereal'
+require 'dotenv'
+Dotenv.load '.env'
 
+require 'sidereal'
+require 'ruby_llm'
+
+Sidereal.configure do |c|
+  c.workers = 3
+end
+
+RubyLLM.configure do |config|
+  # Add keys ONLY for the providers you intend to use.
+  # Using environment variables is highly recommended.
+  config.openai_api_key = ENV.fetch('OPENAI_API_KEY')
+  # config.anthropic_api_key = ENV.fetch('ANTHROPIC_API_KEY')
+end
 # -- Messages --
 
 SendMessage = Sidereal::Message.define('chat.send_message') do
+  attribute :author, Sidereal::Types::String.present
+  attribute :body, Sidereal::Types::String.present
+end
+
+AskLLM = Sidereal::Message.define('chat.ask_llm') do
   attribute :author, Sidereal::Types::String.present
   attribute :body, Sidereal::Types::String.present
 end
@@ -27,10 +46,23 @@ class ChatApp < Sidereal::App
 
   command SendMessage do |cmd|
     MESSAGES << cmd
+    if cmd.payload.body.to_s =~ /@bot /
+      dispatch AskLLM, cmd.payload
+    end
     dispatch ChatNotify, message: "#{cmd.payload.author}: #{cmd.payload.body}"
   end
 
   command ChatNotify do |cmd|
+  end
+
+  command AskLLM do |cmd|
+    chat = RubyLLM.chat
+    begin
+      response = chat.ask(cmd.payload.body)
+    rescue Exception => ee
+      Console.warn ee
+    end
+    dispatch SendMessage, author: 'Bot', body: response.content
   end
 
   page ChatPage
